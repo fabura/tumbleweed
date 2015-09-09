579aef6ff39dc656b47a6aa4fdbe951e0e55d442
Паша Иванов <pashaivanov@MacBook-Air-Pasha.local>
2015-09-09 19:11:35 +0300

    Almost done

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,70 @@
 (function () {
-    var $slider = $('.slider');
+    var slider = (function () {
+        var $slider = $('.slider');
+        var $slides = $slider.find('.slide');
+        var $loco = $slides.eq(0);
+        var $indexes = $slider.find('.index');
 
-    $slider.find('.arrow-right').click(function () {
-      $slider.find('.slide:first').css('margin-left', '-100%');
+        var index = 0;
+
+        var showSlide = function (index) {
+            $loco.css('margin-left', '-' + (index * 100) + '%');
+            $indexes
+                .removeClass('active')
+                .eq(index).addClass('active');
+        };
+
+        var boundIndex = function () {
+            if (index === $slides.length) { index = 0; }
+            if (index === -1) { index = $slides.length - 1; }
+        };
+
+        var next = function () {
+            index++;
+            boundIndex();
+            showSlide(index);
+        };
+
+        var prev = function () {
+            index--;
+            boundIndex();
+            showSlide(index);
+        };
+
+        showSlide(0);
+
+        $indexes.click(function () {
+            index = $indexes.index($(this));
+            showSlide(index);
+        });
+
+        $slider.find('.arrow-right').click(function () {
+            next();
+        });
+
+        $slider.find('.arrow-left').click(function () {
+            prev();
+        });
+    })();
+
+
+
+    var $form = $('.email-form');
+
+    $form.find('form').on('submit', function (event) {
+        var $email = $form.find('input');
+        var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+
+        if (re.test($email.val())) {
+
+        } else {
+            $form.addClass('invalid');
+
+            setTimeout(function () {
+                $form.removeClass('invalid');
+            }, 1000);
+
+            event.preventDefault();
+        }
     });
 })();
